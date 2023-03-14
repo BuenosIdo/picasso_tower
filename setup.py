e1b6965c4ad6ea09a49a893e0c0215692d7f85b9
Ido Buenos <buenos.work@gmail.com>
2023-03-14 21:28:08 +0200

    Basic package structure with the main picasso tower class

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="picasso_tower",
+      version="0.0.1",
+      author="Ido Buenos",
+      author_email="buenos.work@gmail.com",
+      packages=["picasso_tower"],
+      requires=["pydantic"],
+      )
